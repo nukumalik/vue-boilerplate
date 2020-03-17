1059934fcb004bcbfe7708d22b0805a5f2d05849
nukumalik <nukumalik@gmail.com>
2020-03-18 00:42:02 +0700

    try to using vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,37 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+	state: {
+		blogs: [
+			{ title: 'A', status: 'review' },
+			{ title: 'B', status: 'publish' },
+			{ title: 'C', status: 'delete' }
+		],
+		count: 0
+	},
+	getters: {
+		getBlogs: state => (status = 'publish') => {
+			return state.blogs.filter(item => item.status === status)
+		}
+	},
+	mutations: {
+		increment(state) {
+			return (state.count += 1)
+		},
+		decrement(state) {
+			return (state.count -= 1)
+		}
+	},
+	actions: {
+		async increment({ commit }) {
+			await commit('increment')
+		},
+		async decrement({ commit }) {
+			await commit('decrement')
+		}
+	},
+	modules: {}
+})
